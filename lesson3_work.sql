220af5947994e6dcddfa39c888b042230f13ceb1
Andei <93673737+andreidfr0@users.noreply.github.com>
2022-05-04 10:48:57 +0300

    add q1.1 work     q1.2 work

--- lesson3_work.sql
+++ lesson3_work.sql
@@ -1,8 +1,8 @@
 -- Практическое задание
---   База данных «Страны и города мира»:
+--  1. База данных «Страны и города мира»:
 --   1. Сделать запрос, в котором мы выберем все данные о городе – регион, страна.
 --   2. Выбрать все города из Московской области.
---   База данных «Сотрудники»:
+--  2. База данных «Сотрудники»:
 --   1. Выбрать среднюю зарплату по отделам.
 --   2. Выбрать максимальную зарплату у сотрудника.
 --   3. Удалить одного сотрудника, у которого максимальная зарплата.
@@ -10,7 +10,7 @@
 --   5. Найти количество сотрудников в отделах и посмотреть, сколько всего денег получает отдел.
 
 use geodata;
-
+-- 1.1
 select
  ci.id AS 'ID',
  ci.country_id AS 'country ID',
@@ -22,4 +22,21 @@ select
 from _cities ci
 	left join _countries co ON ci.country_id = co.id
     left join _regions re ON ci.region_id = re.id
-limit 10000;
+;
+
+-- 1.2
+select
+ ci.*,
+ re.title AS region
+from _cities ci
+    left join _regions re ON ci.region_id = re.id
+where
+	re.title like 'московская%'
+;
+-- order by ci.title asc
+-- limit 10
+;
+-- select 
+-- count(*) as 'all_rows'
+-- from _cities
+;
